6f57ec944cdd5cd1551f4b5734290a755b4f8a5b
Siddhanth <sid.samarai@gmail.com>
2026-04-04 14:43:28 +0530

    update portfolio

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,14 +1,16 @@
 export default function Experience() {
   return (
-    <section className="py-20 text-center">
-      <h2 className="text-3xl font-bold mb-10">Experience</h2>
+    <section className="bg-[#ddd6c8] dark:bg-[#111] py-20 transition-all duration-500">
+  <h2 className="text-3xl font-bold text-center mb-10">Experience</h2>
 
-      <div className="max-w-2xl mx-auto bg-[#1a1a1a] p-6 rounded-xl border border-[#333]">
-        <h3 className="text-xl font-semibold">Frontend Intern</h3>
-        <p className="text-gray-400 mt-2">
-          Built responsive UI using React & Tailwind. Improved UX and performance.
-        </p>
-      </div>
-    </section>
-  );
+  <div className="max-w-3xl mx-auto">
+    <div className="bg-[#f5f1e8] dark:bg-[#1a1a1a] text-black dark:text-white shadow-xl rounded-2xl p-8 text-center transition-all duration-500">
+      <h3 className="text-xl font-semibold mb-2">Frontend Intern</h3>
+      <p className="text-gray-600 dark:text-gray-400">
+        Built responsive UI using React & Tailwind. Improved UX and performance.
+      </p>
+    </div>
+  </div>
+</section>
+  )
 }
